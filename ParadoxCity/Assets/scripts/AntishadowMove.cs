74bad8df7f542092d70992d8e9e6bedf6e9166f7
hmmkhm <wakarumasen@gmail.com>
2022-01-30 01:18:10 +0300

    CharacterAmovementDone

--- ParadoxCity/Assets/scripts/AntishadowMove.cs
+++ ParadoxCity/Assets/scripts/AntishadowMove.cs
@@ -6,7 +6,9 @@ public class AntishadowMove : MonoBehaviour
 {
 	public Rigidbody2D rb;
 	public Vector2 moveVector;
-	public float speed = 2f;
+	public int speed = 2;
+	public int fastSpeed = 4;
+	private int realSpeed;
 	public Animator anim;
 	public SpriteRenderer sr;
 	public bool faceRight = true;
@@ -15,21 +17,24 @@ public class AntishadowMove : MonoBehaviour
 		rb = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 		sr = GetComponent<SpriteRenderer> ();
+		realSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
 		Walk ();
-		//Flip ();
+		Run ();
 		Reflect();
+		Jump ();
+		CheckingGround ();
 
     }
 	void Walk()
 	{
 		moveVector.x = Input.GetAxis("Horizontal");
 		anim.SetFloat ("moveX", Mathf.Abs (moveVector.x));
-		rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+		rb.velocity = new Vector2(moveVector.x * realSpeed, rb.velocity.y);
 
 	}
 
@@ -41,16 +46,40 @@ public class AntishadowMove : MonoBehaviour
 			faceRight = !faceRight;
 		}
 	}
+	public float jumpForce = 50f;
 
-	void Flip()
+
+	void Jump()
 	{
-		//if (moveVector.x > 0) 
-		//{
-		//	sr.flipX = false;
-		//} else if (moveVector.x < 0) 
-		//{
-		//	sr.flipX - true;
-		//}
-		sr.flipX = moveVector.x < 0;
+		if (Input.GetKeyDown (KeyCode.UpArrow) && onGround) 
+		{
+			//rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+			rb.AddForce(Vector2.up * jumpForce);
+		}
 	}
+	public bool onGround;
+	public Transform GroundCheck;
+	public float checkRadius = 0.5f;
+	public LayerMask Ground;
+
+	void CheckingGround ()
+	{
+		onGround = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, Ground);
+		anim.SetBool ("onGround", onGround);
+	}
+	void Run()
+	{
+		if (Input.GetKey (KeyCode.LeftShift)) 
+		{
+			anim.SetBool ("run", true);
+			realSpeed = fastSpeed;
+		} 
+		else 
+		{
+			anim.SetBool ("run", false);
+			realSpeed = speed;
+		}
+	}
+
+
 }
